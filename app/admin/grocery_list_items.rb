39a1f2c87ea6d90aa017bb9864b4cc02caf46f5f
anonymous anonymous <john.suttle@kellogg.northwestern.edu>
2021-12-07 19:08:28 +0000

    Whitelist columns in GroceryListItem for ActiveAdmin

--- app/admin/grocery_list_items.rb
+++ app/admin/grocery_list_items.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register GroceryListItem do
 
+ permit_params :food_id, :purchased_status, :food_item, :note
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
