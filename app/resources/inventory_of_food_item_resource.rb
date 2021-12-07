b1f11f421899a15c8d96bb60fe203f2430455823
anonymous anonymous <john.suttle@kellogg.northwestern.edu>
2021-12-07 19:09:17 +0000

    Generate graphiti resources for inventory_of_food_items

---
+++ app/resources/inventory_of_food_item_resource.rb
@@ -0,0 +1,17 @@
+class InventoryOfFoodItemResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :user_id, :integer
+  attribute :date_purchased, :date
+  attribute :expiration_date, :date
+  attribute :days_until_expiration, :string
+  attribute :purchased_status, :string_enum, allow: InventoryOfFoodItem.purchased_statuses.keys
+  attribute :food_item, :string
+  attribute :note, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
