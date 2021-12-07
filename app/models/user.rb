2c4c8bf24b3cc86d668ddeb29c7c72f67c888b10
anonymous anonymous <john.suttle@kellogg.northwestern.edu>
2021-12-07 19:08:04 +0000

    Direct association between users and inventory_of_food_items

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :inventory_of_food_items,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe User, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:inventory_of_food_items) }
+
     end
 
     describe "InDirect Associations" do
